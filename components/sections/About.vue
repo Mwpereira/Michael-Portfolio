2797e19c1cb3f0e8aac034bfb119b282fd30689e
Michael Pereira <michael.w.pereira.7@gmail.com>
2021-08-22 00:55:33 -0400

    Moving certificates section

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -16,50 +16,41 @@
         <p>My preferred technologies include:</p>
         <div id='technologies'>
           <p class='mt-4'>
-            <i class="far fa-file-code"></i>
-            <i class="fas fa-angle-right"></i>
+            <i class='far fa-file-code'></i>
+            <i class='fas fa-angle-right'></i>
             Typescript
-            <i class="fas fa-angle-right"></i>
+            <i class='fas fa-angle-right'></i>
             Java
-            <i class="fas fa-angle-right"></i>
+            <i class='fas fa-angle-right'></i>
             Vue
-            <i class="fas fa-angle-right"></i>
+            <i class='fas fa-angle-right'></i>
             React
           </p>
           <p class='mt-3'>
-            <i class="fab fa-medapps"></i>
-            <i class="fas fa-angle-right"></i>
+            <i class='fab fa-medapps'></i>
+            <i class='fas fa-angle-right'></i>
             AWS
-            <i class="fas fa-angle-right"></i>
+            <i class='fas fa-angle-right'></i>
             Git
-            <i class="fas fa-angle-right"></i>
+            <i class='fas fa-angle-right'></i>
             Figma
-            <i class="fas fa-angle-right"></i>
+            <i class='fas fa-angle-right'></i>
             Linux
           </p>
           <p class='mt-3'>
-            <i class="fas fa-database"></i>
-            <i class="fas fa-angle-right"></i>
+            <i class='fas fa-database'></i>
+            <i class='fas fa-angle-right'></i>
             MongoDB
-            <i class="fas fa-angle-right"></i>
+            <i class='fas fa-angle-right'></i>
             DynamoDB
-            <i class="fas fa-angle-right"></i>
+            <i class='fas fa-angle-right'></i>
             MSSQL
           </p>
         </div>
       </div>
-      <div class='column has-text-centered'>
+      <div class='column has-text-centered mb-6'>
         <div class='img-container'>
-          <img src='/mwp.jpg' alt='Michael Pereira'/>
-        </div>
-      </div>
-    </div>
-    <div class='my-6'>
-      <p class='is-size-4 mb-5'>Certifications</p>
-      <div class='columns is-vcentered is-multiline'>
-        <div v-for='certificate in certificates' :key='certificate' class='column is-half-tablet-only is-half-desktop-only is-6-widescreen is-6-fullhd'>
-          <CertificationCard :title='certificate.title' :credential-id='certificate.credentialId'
-                             :date-issued='certificate.dateIssued' :provider='certificate.provider' :link='certificate.link' />
+          <img src='/mwp.jpg' alt='Michael Pereira' />
         </div>
       </div>
     </div>
@@ -69,16 +60,9 @@
 <script lang='ts'>
 import { Vue } from 'vue-property-decorator'
 import Component from 'vue-class-component'
-import CertificationCard from '~/components/general/CertificationCard.vue'
-import certificates from '@/assets/json/certifications.json'
 
-@Component({
-  components: { CertificationCard }
-})
+@Component
 export default class About extends Vue {
-  public providers = ['Udemy', 'LinkedIn']
-
-  public certificates = certificates
 }
 </script>
 
--- components/sections/Projects.vue
+++ components/sections/Projects.vue
@@ -2,9 +2,18 @@
   <div id='projects' class='section my-6'>
     <p class='title'>Side Projects</p>
     <hr>
-    <div class='columns is-vcentered is-multiline my-6'>
+    <div class='columns is-vcentered is-multiline mt-6'>
       <div v-for='project in projects' :key='project.name' class='column is-half-tablet-only is-half-desktop-only'>
         <ProjectCard :project='project.name' :desc='project.desc' :tags='project.tags' :github='project.github' :link='project.link' />
+      </div>
+    </div>
+    <div class='my-6'>
+      <p class='is-size-4 mb-5'>Certifications</p>
+      <div class='columns is-vcentered is-multiline'>
+        <div v-for='certificate in certificates' :key='certificate' class='column is-half-tablet-only is-half-desktop-only is-6-widescreen is-6-fullhd'>
+          <CertificationCard :title='certificate.title' :credential-id='certificate.credentialId'
+                             :date-issued='certificate.dateIssued' :provider='certificate.provider' :link='certificate.link' />
+        </div>
       </div>
     </div>
   </div>
@@ -14,13 +23,17 @@
 import { Vue } from 'vue-property-decorator'
 import Component from 'vue-class-component'
 import ProjectCard from '~/components/general/ProjectCard.vue'
+import CertificationCard from '~/components/general/CertificationCard.vue'
 import projects from '@/assets/json/projects.json'
+import certificates from '@/assets/json/certifications.json'
 
 @Component({
-  components: { ProjectCard }
+  components: { CertificationCard, ProjectCard }
 })
 export default class Projects extends Vue {
   public projects = projects
+  public providers = ['Udemy', 'LinkedIn']
+  public certificates = certificates
 }
 </script>
 
