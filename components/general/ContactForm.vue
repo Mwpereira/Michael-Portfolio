fd4eefa62f7260c6685a616cf051d1765979a4de
Michael Pereira <michael.w.pereira.7@gmail.com>
2021-08-26 21:39:44 -0400

    Revert "Trying to fix netlify form"
    This reverts commit 613c00389009d5e78c88556badafb8ba8e19ced1.

--- components/general/ContactForm.vue
+++ components/general/ContactForm.vue
@@ -2,15 +2,15 @@
   <section class='box p-5'>
     <form class='form' name='contact' method='POST' data-netlify='true' data-netlify-honeypot='bot-field' netlify>
       <b-field label='Name'>
-        <input class='input' placeholder='name' type='text' name='Name' />
+        <b-input placeholder='name' type='text' name='Name' />
       </b-field>
 
       <b-field label='Email' class='mt-5 mb-5'>
-        <input class='input' placeholder='email' type='email' name='Email' />
+        <b-input placeholder='email' type='email' name='Email' />
       </b-field>
 
       <b-field label='Message' class='mb-1'>
-        <input class='input' maxlength='250' type='textarea' name='Message' placeholder='message' />
+        <b-input maxlength='250' type='textarea' name='Message' placeholder='message' />
       </b-field>
 
       <button class='button is-dark'>Send Message</button>
