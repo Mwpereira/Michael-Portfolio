2ed6471db7a7150b2ccdf0e6d9a4200dbf1819d2
Michael Pereira <michael.w.pereira.7@gmail.com>
2021-08-26 21:59:43 -0400

    Fixing netlify issues

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  mode: 'spa',
+  mode: 'universal',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
