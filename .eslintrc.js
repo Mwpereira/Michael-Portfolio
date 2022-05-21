fb4b72306852a76d398b350e10b50f02f5406b58
Michael Pereira <michael.w.pereira.7@gmail.com>
2022-05-21 15:43:29 -0400

    updating packages, updating package-lock.json, updating certifications, adding .nvmrc file,  updating lint settings, linting files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,32 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: [
+		'@nuxtjs/eslint-config-typescript',
+		'plugin:nuxt/recommended',
+		'prettier'
+	],
+	plugins: [],
+	rules: {
+		indent: [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		quotes: [
+			'error',
+			'single'
+		],
+		semi: [
+			'error',
+			'always'
+		],
+		'no-unused-vars': 'warn'
+	}
+};
