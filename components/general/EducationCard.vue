c1b856051523031f83f3eefd87f82fe5cdb21ea3
Michael Pereira <mwpereira07@gmail.com>
2023-06-26 00:06:32 -0400

    Updating projects and adding in education

---
+++ components/general/EducationCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="box p-5">
+        <p class="has-text-weight-bold is-size-5">
+            {{ school }}
+        </p>
+        <p class="mt-2 mb-3">{{ location }}</p>
+        <p class="card-desc desc">
+            {{ major }}, {{ degree }}
+            <hr>
+            Date of Graduation: {{ graduation }}
+        <br>
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class EducationCard extends Vue {
+    @Prop()
+    public school!: string
+
+    @Prop()
+    public location!: string
+
+    @Prop()
+    public degree!: string
+
+    @Prop()
+    public major!: string
+    
+    @Prop()
+    public graduation!: string
+}
+</script>
+
+<style scoped>
+button {
+    font-size: 14px;
+}
+</style>
--- components/sections/Work.vue
+++ components/sections/Work.vue
@@ -2,6 +2,7 @@
     <div id="work" class="section my-6">
         <p class="title">Experience</p>
         <hr />
+        <p class="subtitle has-text-weight-normal is-italic">Work</p>
         <div v-for="place in work" :key="place.company" class="mb-6">
             <WorkCard
                 :company="place.company"
@@ -11,6 +12,17 @@
                 :reference-letter="place.referenceLetter"
             />
         </div>
+        <br>
+        <p class="subtitle has-text-weight-normal is-italic">Education</p>
+        <div v-for="school in education" :key="school.school" class="mb-6">
+            <EducationCard
+                :school="school.school"
+                :location="school.location"
+                :degree="school.degree"
+                :major="school.major"
+                :graduation="school.graduation"
+            />
+        </div>
     </div>
 </template>
 
@@ -18,12 +30,15 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 import work from '@/content/json/work.json'
 import WorkCard from '~/components/general/WorkCard.vue'
+import education from '@/content/json/education.json'
+import EducationCard from '../general/EducationCard.vue';
 
 @Component({
-    components: { WorkCard },
+    components: { EducationCard, WorkCard },
 })
 export default class Work extends Vue {
     public work = work
+    public education = education
 }
 </script>
 
