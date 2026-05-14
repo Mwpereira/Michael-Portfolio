21f7dcec394d32557ecac056856524384e81b474
Michael Pereira <mwpereira07@gmail.com>
2026-05-14 00:34:32 -0400

    chore: cleaning up netlify configs, updating imports for theme-provider.tsx component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'avatars.githubusercontent.com'],
     remotePatterns: [
       {
         protocol: 'https',
